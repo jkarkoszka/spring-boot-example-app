ec7d75b73d7c85f8e4e81af9f166d00e688c8884
Jakub Karkoszka <jakub.karkoszka@wipro.pl>
2019-06-28 16:11:54 +0200

    Added more replicas and host info to response

--- src/main/java/com/jkarkoszka/springbootexample/HelloController.java
+++ src/main/java/com/jkarkoszka/springbootexample/HelloController.java
@@ -3,11 +3,22 @@ package com.jkarkoszka.springbootexample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HelloController {
 
     @GetMapping(value = "/")
     public String hello() {
-        return "Hello world!";
+        return "Hello world  from " + getLocalHost();
+    }
+
+    private String getLocalHost() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown host";
+        }
     }
 }
